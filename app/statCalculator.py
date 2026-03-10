11ca6afd85bf8ba5789a50f0198ed187dc18954e
Eli-i-z <elizirkle01@gmail.com>
2026-03-10 13:30:05 -0700

    Update statCalculator.py

--- app/statCalculator.py
+++ app/statCalculator.py
@@ -3,26 +3,27 @@ import pandas as pd
 # Load in the data with read_csv()
 # TODO #1: change the file name to your data file name
 playerData = pd.read_csv("app/nhldraft.csv", header=0)   # identify the header row
+cleanedPlayerData = playerData.dropna(subset=['goals', 'assists'])
 
 def getDefensiveGoals():
-    defensivePlayers = playerData[playerData.iloc[:, 6] == "D"]
+    defensivePlayers = cleanedPlayerData[cleanedPlayerData.iloc[:, 6] == "D"]
     defensiveGoals = defensivePlayers.iloc[:, 11].sum()
     return defensiveGoals
 def getOffensiveGoals():
-    offensivePlayers = playerData[playerData.iloc[:, 6] != "D"]
+    offensivePlayers = cleanedPlayerData[cleanedPlayerData.iloc[:, 6] != "D"]
     offensiveGoals = offensivePlayers.iloc[:, 11].sum()
     return offensiveGoals
 def getOffensivePlayers():
-     offensivePlayers = len(playerData[playerData.iloc[:, 6] != "D"])
+     offensivePlayers = len(cleanedPlayerData[playerData.iloc[:, 6] != "D"])
      return offensivePlayers
 def getDefensivePlayers():
-     defensivePlayers = len(playerData[playerData.iloc[:, 6] == "D"])
+     defensivePlayers = len(cleanedPlayerData[cleanedPlayerData.iloc[:, 6] == "D"])
      return defensivePlayers
 def getDeffensiveAssists():
-    defensivePlayers = playerData[playerData.iloc[:, 6] == "D"]
+    defensivePlayers = cleanedPlayerData[cleanedPlayerData.iloc[:, 6] == "D"]
     defensiveAssists = defensivePlayers.iloc[:, 12].sum()
     return defensiveAssists
 def getOffensiveAssists():
-    offensivePlayers = playerData[playerData.iloc[:, 6] != "D"]
+    offensivePlayers = cleanedPlayerData[cleanedPlayerData.iloc[:, 6] != "D"]
     offensiveAssists = offensivePlayers.iloc[:, 12].sum()
     return offensiveAssists
